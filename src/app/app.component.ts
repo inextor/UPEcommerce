46467de05c61b88e12d05547b0765d1848c6264d
nextor <nextor@gmail.com>
2025-04-02 23:49:07 -0700

    Adding first versions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { RestService } from './modules/shared/services/rest.service';
+import { SharedModule } from './modules/shared/shared.module';
+import { ConfirmationService } from './modules/shared/services/confirmation.service';
+import { KeyboardShortcutEvent } from './modules/shared/Utils';
+import { ModalComponent } from './components/modal/modal.component';
+import { RestSimple } from './modules/shared/services/Rest';
+import { HttpClientModule } from '@angular/common/http';
+import { Store } from './modules/shared/RestModels';
+//import { HeaderComponent } from './components/header/header.component';
+import { BuildInfo } from './modules/shared/BuildInfo';
+//import { PageStructureComponent } from "./modules/shared/page-structure/page-structure.component";
 
-@Component({
+@Component
+({
   selector: 'app-root',
-  imports: [RouterOutlet],
+    standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, RouterModule, SharedModule, ModalComponent  ]
 })
-export class AppComponent {
-  title = 'UPClientPages';
+export class AppComponent implements OnInit {
+	title = 'POSProcesos';
+	store_name:string = '';
+
+	build_info = BuildInfo;
+
+	constructor(protected rest:RestService, public confirmation:ConfirmationService, public router:Router)
+	{
+
+	}
+
+	ngOnInit(): void
+	{
+		if( this.rest.user )
+		{
+			console.log('FOOO user', this.rest.user);
+			this.router.navigate(['/dashboard']);
+		}
+		else
+		{
+			console.log('NO user');
+			this.router.navigate(['/login']);
+		}
+	}
+
+	keyHandler(kse:KeyboardShortcutEvent)
+	{
+
+		if( this.confirmation.show_confirmation )
+		{
+			if( kse.shortcut.key_combination == 'Enter' )
+			{
+				kse.stopPropagation();
+				this.confirmation.onAccept();
+				return;
+			}
+
+			if( kse.shortcut.key_combination == 'Escape' )
+			{
+				kse.stopPropagation();
+				this.confirmation.onCancel();
+			}
+		}
+	}
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
+//import { AppRoutingModule } from './app-routing.module';
+import { routes } from './app-routing.module';
+import { provideHttpClient } from '@angular/common/http';
 
-import { routes } from './app.routes';
+//import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+	//Original
+	//providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+	//Nextor
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter( routes, withHashLocation() ),
+		provideHttpClient()]
 };
